8c2122643017e463507547da9d66b288547041ce
Emily Trifone <emilytrifone@gmail.com>
2024-07-05 00:33:44 -0400

    updated database script

--- AccountingLedgerSpring/database/accounting_ledger_db.sql
+++ AccountingLedgerSpring/database/accounting_ledger_db.sql
@@ -22,6 +22,7 @@ PRIMARY KEY(`transaction_id`)
 TRUNCATE TABLE transactions;
 
 -- Add data to transactions
-
-INSERT INTO transactions (description, vendor, amount) VALUES ("Invoice","Bank of America", 2200.0);
-INSERT INTO transactions (description, vendor, amount) VALUES ("Bill", "Mortgages of America", -450.0);
+INSERT INTO transactions (date_time, description, vendor, amount) VALUES ('2023-07-02 16:34:55', "Heating Pad", "Amazon", -25.99);
+INSERT INTO transactions (date_time, description, vendor, amount) VALUES ('2024-06-02 16:34:55', "Harney & Sons - Hot Cinnamon Sunset", "Amazon", -5.99);
+INSERT INTO transactions (description, vendor, amount) VALUES ("Invoice", "Bank of America", 2200.0);
+INSERT INTO transactions (description, vendor, amount) VALUES ("Bill", "Cooper Mortgages", -450.0);
